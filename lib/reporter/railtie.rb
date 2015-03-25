342e1e1773463f9d7b865012f7d53c42b41752c5
Rohit Bhore <rohitpbhore@gmail.com>
2015-03-25 12:39:48 +0530

    rename it !!!

--- lib/reporter/railtie.rb
+++
@@ -1,8 +0,0 @@
-module AppReporter
-  class Railtie < Rails::Railtie
-  	rake_tasks do
-    	spec = Gem::Specification.find_by_name 'app_reporter'
-			load "#{spec.gem_dir}/lib/tasks/app_reporter.rake"
-  	end
-  end
-end
